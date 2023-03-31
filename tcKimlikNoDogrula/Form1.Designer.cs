7ce77b45f9c14cd2aee314887af214c3e23d2391
Furkan Ertekin <furkanertekin0@gmail.com>
2023-03-31 16:06:09 +0300

    dogrulama

---
+++ tcKimlikNoDogrula/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace tcKimlikNoDogrula
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnKontrolet = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIsim = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSoyisim = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDogumYili = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtTCKimlikNo = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtDogumYili);
+            this.groupBox1.Controls.Add(this.txtSoyisim);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtTCKimlikNo);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtIsim);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(195, 205);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Kişisel Bilgiler";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnKontrolet);
+            this.groupBox2.Location = new System.Drawing.Point(214, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(117, 206);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "İşlem";
+            // 
+            // btnKontrolet
+            // 
+            this.btnKontrolet.Location = new System.Drawing.Point(6, 19);
+            this.btnKontrolet.Name = "btnKontrolet";
+            this.btnKontrolet.Size = new System.Drawing.Size(105, 23);
+            this.btnKontrolet.TabIndex = 0;
+            this.btnKontrolet.Text = "Kontrol Et";
+            this.btnKontrolet.UseVisualStyleBackColor = true;
+            this.btnKontrolet.Click += new System.EventHandler(this.btnKontrolet_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "İsim";
+            // 
+            // txtIsim
+            // 
+            this.txtIsim.Location = new System.Drawing.Point(6, 80);
+            this.txtIsim.Name = "txtIsim";
+            this.txtIsim.Size = new System.Drawing.Size(183, 20);
+            this.txtIsim.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Soyisim";
+            // 
+            // txtSoyisim
+            // 
+            this.txtSoyisim.Location = new System.Drawing.Point(6, 120);
+            this.txtSoyisim.Name = "txtSoyisim";
+            this.txtSoyisim.Size = new System.Drawing.Size(183, 20);
+            this.txtSoyisim.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Doğum Yılı";
+            // 
+            // txtDogumYili
+            // 
+            this.txtDogumYili.Location = new System.Drawing.Point(5, 159);
+            this.txtDogumYili.Name = "txtDogumYili";
+            this.txtDogumYili.Size = new System.Drawing.Size(184, 20);
+            this.txtDogumYili.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "T.C. Kimlik Numarası";
+            // 
+            // txtTCKimlikNo
+            // 
+            this.txtTCKimlikNo.Location = new System.Drawing.Point(6, 41);
+            this.txtTCKimlikNo.Name = "txtTCKimlikNo";
+            this.txtTCKimlikNo.Size = new System.Drawing.Size(183, 20);
+            this.txtTCKimlikNo.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(343, 227);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "T.C. Kimlik No Doğrulama";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtSoyisim;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtIsim;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnKontrolet;
+        private System.Windows.Forms.TextBox txtDogumYili;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTCKimlikNo;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
